70eee02361393efa0d80b2de48317ebf5379ce78
Mariakhan4621 <160171512+Mariakhan4621@users.noreply.github.com>
2024-03-15 18:32:02 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,19 @@
+type car = {
+    manufacture: string
+    model: string
+    [key: string]: any;
+}
+
+function createCar(manufacture: string, model: string, optional: Record<string, any>): car {
+return{
+    manufacture,
+    model,
+    ...optional
+}
+
+}
+
+const mycar: car = createCar("toyoto","corola", {color: "silver" , year: "2024"})
+console.log(mycar)
+
+
